89fd8e7d821b5439dd92b932939b2ffae7c44bbd
Nabil <nabilhossainprf@gmail.com>
2025-09-30 17:04:31 +0600

    navbar changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -43,19 +43,19 @@ export default function NavBar() {
             {isShopOpen && (
               <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
                 <Link
-                  href="#"
+                  href="/shop"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Men
                 </Link>
                 <Link
-                  href="#"
+                  href="/shop"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Women
                 </Link>
                 <Link
-                  href="#"
+                  href="/shop"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Accessories
